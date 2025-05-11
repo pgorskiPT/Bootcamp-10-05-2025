79b84b9d52371f1b42aa88feeb93db53f27e92b1
radoslawjaniak <rajkokret660@gmail.com>
2025-05-11 13:54:57 +0200

    [2 12] update typy_danych_3_krotka.py

--- day_2_11_05_2025/typy_danych_3_krotka.py
+++ day_2_11_05_2025/typy_danych_3_krotka.py
@@ -36,7 +36,26 @@ print(tupla_liczby)
 # temp[0] = 1  # TypeError: 'tuple' object does not support item assignment
 # ctrl / - automatyczny komentarz
 
+# nie da się usunąc jednego elementu z krotki
+# del temp[0] # TypeError: 'tuple' object doesn't support item deletion
+
 # można usunąć całą tuple z pamięci
 del temp
 # print(temp) # NameError: name 'temp' is not defined
 # przerwa do 13:45
+
+print(tupla_liczby)  # (43, 55, 22.34, 11, 200)
+# spróbujcuie czy na tupli działa slicowanie
+print(tupla_liczby[:3])  # (43, 55, 22.34)
+print(tupla_liczby[0:3])  # (43, 55, 22.34)
+print(tupla_liczby[1:3])  # (55, 22.34)
+print(tupla_liczby[2:])  # (22.34, 11, 200)
+
+print(tupla_liczby[-1])  # 200
+print(tupla_liczby[::-1])  # (200, 11, 22.34, 55, 43)
+print(tupla_liczby[-1::-1])  # (200, 11, 22.34, 55, 43)
+# [start:stop:krok] krok -1 oznacza krok do tyłu
+print(tupla_liczby[1:4:2])  # (55, 11) co drugi element
+print(tupla_liczby[:])  # (43, 55, 22.34, 11, 200)
+
+
