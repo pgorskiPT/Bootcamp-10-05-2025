2dffa395f2eaefa3c1c95e7bb620ba958f67deda
radoslawjaniak <rajkokret660@gmail.com>
2025-05-24 15:37:03 +0200

    [3 27] add wyjatki_zad_kalkulator.py

---
+++ day_3_24_05_2025/wyjatki_zad_kalkulator.py
@@ -0,0 +1,7 @@
+# napisac program kalkulator z wykorzystaniem pętli while True
+# przechwycić wyjątki i obsłużyc
+# ładnie wypisać wynik np.: Dodawanie 2 + 4 = 6
+
+# wyswietlic menu z działaniami
+# pobrac dane
+# wyświetlic wynik wybranego działania
