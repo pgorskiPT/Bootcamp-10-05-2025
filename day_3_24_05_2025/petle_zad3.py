4a9cb16c3762b52e87c69f4d3a523476b06e177b
radoslawjaniak <rajkokret660@gmail.com>
2025-05-24 15:11:44 +0200

    [3 25] update petle_zad3.py

--- day_3_24_05_2025/petle_zad3.py
+++ day_3_24_05_2025/petle_zad3.py
@@ -17,12 +17,32 @@ while licznik < 15:
     licznik += 1
     print("Komunikat 2 !!!")
 
-password = input("Podaj hasło")
-while password != 'secret':
-    password = input("Błędne hasło. podaj ponownie")
-print("Hasło prawidłowe")
+# password = input("Podaj hasło")
+# while password != 'secret':
+#     password = input("Błędne hasło. podaj ponownie")
+# print("Hasło prawidłowe")
 # Podaj hasłosadfa
 # Błędne hasło. podaj ponownieasdasfa
 # Błędne hasło. podaj ponownieeafafa
 # Błędne hasło. podaj ponowniesecret
 # Hasło prawidłowe
+
+lista = []
+lista_int = []
+while True:
+    wej = input("Podaj liczbę")
+    # A string is numeric if all characters in the string are numeric
+    if not wej.isnumeric():
+        break
+    lista.append(wej)
+    lista_int.append(int(wej))
+
+print(lista)  # ['1', '2', '3', '4', '5', '6']
+print(lista_int)  # [1, 2, 3, 4, 5, 6]
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+element_to_remove = 5
+while element_to_remove in my_list:
+    my_list.remove(element_to_remove)
+
+print(my_list)  # [1, 2, 3, 4, 6]
