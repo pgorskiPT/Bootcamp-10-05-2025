ba7e7fd33c2133c84cb40de2151422e5af26613d
radoslawjaniak <rajkokret660@gmail.com>
2025-05-24 10:58:15 +0200

    [3 7] update warunki.py

--- day_3/warunki.py
+++ day_3/warunki.py
@@ -33,3 +33,26 @@ if odp == "Radek":
     print("Radek")
 else:  # w przeciwnym przypadku
     print("Inny pacjent")  # Inny pacjent
+
+podatek = 0.9
+zarobki = int(input("Podaj dochód"))
+# pop ierwszym spełnionym warunku wychodzi z drzewka
+# kolejnośc warunków ma znaczenie
+# elif - używamy po to by sprawdzić dokładnie jeden warunek
+if zarobki < 10000:
+    podatek = 0.0
+elif zarobki < 30_000:
+    podatek = 0.2
+elif zarobki < 100_000:
+    podatek = 0.4
+else:  # działannie domyślne
+    podatek = 0.9
+
+print(f"Płacisz {zarobki * podatek} podatek")
+
+# tu ostatni nadpisze poprzednie jesli będzie spełniony
+if zarobki < 10000:
+    podatek = 0
+
+if zarobki < 1999:
+    podatek = 0.4
