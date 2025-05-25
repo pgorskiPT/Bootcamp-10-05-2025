f5f815a1ae9ae8be14d342ae3cf1747aa3513a08
radoslawjaniak <rajkokret660@gmail.com>
2025-05-25 15:37:09 +0200

    [4 21] add csv_zad2.py

--- day_4_25_05_2025/csv_zad1.py
+++ day_4_25_05_2025/csv_zad1.py
@@ -5,6 +5,7 @@
 # "Jan Wiśniewski","fajny","2"
 # Kowalski,"wiecznie pyta ""która godzina"", ale może być",5
 import csv  # biblioteka do działan z plikami csv
+from datetime import date, timedelta
 
 row = ["Radek", 'Coe', "3", 0]
 fields = ['name', 'branch', 'year', 'cgpa']
@@ -26,4 +27,21 @@ with open("dane/records_2.csv", "w", newline="") as f:
 with open("dane/records_3.csv", "w", newline="") as csv_f:
     csv_dict_writer = csv.DictWriter(csv_f, fieldnames=fields)
     csv_dict_writer.writeheader()  # zapisz nazwy kolumn
-    csv_dict_writer.writerow(zipped_dict) # zapis dannych z jedego słownika
+    csv_dict_writer.writerow(zipped_dict)  # zapis dannych z jedego słownika
+
+today = date.today()
+tomorrow = today + timedelta(days=1)
+
+products = [
+    {"sku": 1, 'exp_date': today, "price": 100},
+    {"sku": 2, 'exp_date': today, "price": 200},
+    {"sku": 3, 'exp_date': tomorrow, "price": 499.99},
+    {"sku": 4, 'exp_date': today, "price": 50},
+    {"sku": 5, 'exp_date': tomorrow, "price": 80},
+]
+# wyciagnięcie kluczy ze słownika umieszczonego w liście
+fields_product = [k for k in products[0]]
+with open("dane/records_discount.csv", 'w', newline="") as discount:
+    csv_dict_writer = csv.DictWriter(discount, fieldnames=fields_product, delimiter=";")
+    csv_dict_writer.writeheader()
+    csv_dict_writer.writerows(products) # z literką 's' bo zapisujemy listę
