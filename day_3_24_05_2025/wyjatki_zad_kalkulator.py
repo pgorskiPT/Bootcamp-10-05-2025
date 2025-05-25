0440b043d4d1bdb17423970ace3e576e506e7ec2
radoslawjaniak <rajkokret660@gmail.com>
2025-05-25 09:19:18 +0200

    [4 1] update wyjatki_zad_kalkulator.py

--- day_3_24_05_2025/wyjatki_zad_kalkulator.py
+++ day_3_24_05_2025/wyjatki_zad_kalkulator.py
@@ -6,3 +6,34 @@
 # pobrac dane
 # wyświetlic wynik wybranego działania
 # ```python```
+
+while True:
+    print("""
+    1. Dodawanie
+    2. Odejmowanie
+    3. Mnożenie
+    4. Dzielenie
+    5. Koniec
+    """)
+
+    odp = input("Podaj opcje menu")  # str
+    if odp not in ["1", "2", "3", "4"]:
+        break
+    try:
+        a = float(input("Podaj pierwszą liczbę"))
+        b = float(input("Podaj drugą liczbę"))
+
+        if odp == "1":
+            print(f"Wynik dodawania {a} + {b} = {a + b}")
+        elif odp == "2":
+            print(f"Wynik odejmowania {a} - {b} = {a - b}")
+        elif odp == "3":
+            print(f"Wynik mnożenia {a} * {b} = {a * b}")
+        elif odp == "4":
+            print(f"Wynik dzielenia {a} / {b} = {a / b}")
+    except ZeroDivisionError:
+        print("Nie dziel przez zero!!!")
+    except Exception as e:
+        print("Bład", e)
+    else:
+        print("Działąnie wykonane poprawnie")
