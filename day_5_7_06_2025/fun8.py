e1522aac9dab5e9b000de03b543f88fe5e5c2e2a
radoslawjaniak <rajkokret660@gmail.com>
2025-06-07 12:03:22 +0200

    [5 12a] update fun8

--- day_5_7_06_2025/fun8.py
+++ day_5_7_06_2025/fun8.py
@@ -13,3 +13,53 @@ allparams(1, 2, c=9)  # 1 2 9
 allparams(1, 2, c=9, a=8)  # {'a': 8}
 allparams(1, 2, 3, a=8, name="Radek")  # 1 2 3,{'a': 8, 'name': 'Radek'}
 
+
+def allparams_all(a, b, /, c=43, *args, d=256, **kwargs):
+    print(f"{a=}, {b=}")
+    print(f"{c=}, {d=}")
+    print(f'{args=}')
+    print(f'{kwargs=}')
+
+
+allparams_all(1, 2)
+# a=1, b=2
+# c=43, d=256
+# args=()
+# kwargs={}
+allparams_all(1, 2, 3)
+# a=1, b=2
+# c=3, d=256
+# args=()
+# kwargs={}
+allparams_all(1, 2, c=90)
+# a=1, b=2
+# c=90, d=256
+# args=()
+# kwargs={}
+# zeby przekazac do args, c musi byc pozycyjne
+allparams_all(1, 2, 90, 2, 4, 5, 6)
+# a=1, b=2
+# c=90, d=256
+# args=(2, 4, 5, 6)
+# kwargs={}
+allparams_all(1, 2, 90, 2, 4, 5, 6, 7, 8, 9, 10)
+# a=1, b=2
+# c=90, d=256
+# args=(2, 4, 5, 6, 7, 8, 9, 10)
+# kwargs={}
+# przy takiej konstrukcji funkcji d możemy przekazac tylko po nazwie
+allparams_all(1, 2, 90, 2, 4, 5, 6, 7, 8, 9, 10, d=100)
+# a=1, b=2
+# c=90, d=100
+# args=(2, 4, 5, 6, 7, 8, 9, 10)
+# kwargs={}
+allparams_all(1, 2, 90, 2, 4, 5, 6, 7, 8, 9, 10, d=100, name="Radek")
+# a=1, b=2
+# c=90, d=100
+# args=(2, 4, 5, 6, 7, 8, 9, 10)
+# kwargs={'name': 'Radek'}
+allparams_all(1, 2, 90, 2, 4, 5, 6, 7, 8, 9, 10, d=100, name="Radek", a=89)
+# a=1, b=2
+# c=90, d=100
+# args=(2, 4, 5, 6, 7, 8, 9, 10)
+# kwargs={'name': 'Radek', 'a': 89} - a trafia do kwargs
