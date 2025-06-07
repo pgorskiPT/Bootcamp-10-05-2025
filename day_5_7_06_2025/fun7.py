7d03d74340fd30542cee9f7cfa1996efde461ecc
radoslawjaniak <rajkokret660@gmail.com>
2025-06-07 11:41:51 +0200

    [5 11] update fun7

--- day_5_7_06_2025/fun7.py
+++ day_5_7_06_2025/fun7.py
@@ -15,3 +15,16 @@ def connect(**opcje):  # ** argumenty nazwane, argumenty słownikowe
 connect()  # {}
 connect(z=9)  # {'z': 9}
 connect(a=9, name="Radek")  # {'a': 9, 'name': 'Radek'}
+
+
+def connect_all(*args, **kwargs):
+    print(args, kwargs)
+
+
+connect_all()  # () {}
+connect_all(1, 2, 3)  # (1, 2, 3) {}
+connect_all(1, 2, 3, 4, 5, 6)  # (1, 2, 3, 4, 5, 6) {}
+connect_all(1, 2, 3, 4, 5, 6, "Zenek")  # (1, 2, 3, 4, 5, 6, 'Zenek') {}
+connect_all(1, 2, 3, 4, 5, 6, "Zenek", a=9, b=89)  # (1, 2, 3, 4, 5, 6, 'Zenek') {'a': 9, 'b': 89}
+connect_all(d=9, name="Tomek")  # () {'d': 9, 'name': 'Tomek'}
+# connect_all(c=9, 1, 2, 3, 4) # SyntaxError: positional argument follows keyword argument
