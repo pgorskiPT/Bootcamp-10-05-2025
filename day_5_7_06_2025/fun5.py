7d10f3347eda22e0c14c8a6480aa96113c1e1e2f
radoslawjaniak <rajkokret660@gmail.com>
2025-06-07 10:50:29 +0200

    [5 7] add fun5 pizzeria

--- day_5_7_06_2025/fun5.py
+++ day_5_7_06_2025/fun5.py
@@ -1,4 +1,40 @@
 # zrobic funkcję restauracja()
 # zamow_pizza, zamow_napoj
-# w zależności od zamówienia ma mzwrócic odpowiednią funkcję
+# w zależności od zamówienia ma zwrócic odpowiednią funkcję
 # użyc tych funkcji w głównym programmie
+
+def restauracja(typ_zamowienia):
+    print("Witamy w naszej restauracji")
+
+    def zamow_pizza(skladniki="pieczarki"):
+        print("Zamówiono pizza, skłądniki:", skladniki)
+
+    def zamow_napoj(nazwa="herbata"):
+        print('Zamow napoj:', nazwa)
+
+    if typ_zamowienia.casefold().strip() == 'pizza':
+        return zamow_pizza
+    else:
+        return zamow_napoj
+
+
+zamowienie_pizza = restauracja('pizza')
+zamowienie_pizza()
+# Witamy w naszej restauracji
+# Zamówiono pizza, skłądniki: pieczarki
+zamowienie_pizza("pieczarki, szynka")
+# Zamówiono pizza, skłądniki: pieczarki, szynka
+
+zamowienie_napoj = restauracja('napoj')
+zamowienie_napoj()
+zamowienie_napoj('cola')
+# Witamy w naszej restauracji
+# Zamow napoj: herbata
+# Zamow napoj: cola
+
+zamowienie_pizza()
+zamowienie_napoj()
+zamowienie_napoj()
+# Zamówiono pizza, skłądniki: pieczarki
+# Zamow napoj: herbata
+# Zamow napoj: herbata
