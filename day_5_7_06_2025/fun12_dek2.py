ce5fbe4b53b1954723e4d36a8d123fd062873b30
radoslawjaniak <rajkokret660@gmail.com>
2025-06-07 14:52:38 +0200

    [5 22] add fun12_dek2.py.py

---
+++ day_5_7_06_2025/fun12_dek2.py
@@ -0,0 +1,34 @@
+# napisać dekorator, który zmieni wynik działania funkcji na duże litery
+
+def uppercase_decorator(func):
+    def wrapper():
+        result = func()
+        return result.upper()
+
+    return wrapper  # adres funkcji
+
+
+# bold_decorator \033[1m , \033[0m
+def bold_decorator(func):
+    def wrapper():
+        result = func()
+        return f"\033[1m" + result + "\033[0m"
+
+    return wrapper
+
+
+@uppercase_decorator
+def greeting():
+    return "Hello World!"
+
+
+# kolejność ma znaczenie
+@bold_decorator
+@uppercase_decorator
+def greeting2():
+    return "Hello World!"
+
+
+print(greeting())  # Hello World! bez dekoratora
+# po uzyciu dekoratora uppercase: HELLO WORLD!
+print(greeting2())
