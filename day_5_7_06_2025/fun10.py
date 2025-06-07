391daaa28078f80adbd7d263bf6d3446712351b3
radoslawjaniak <rajkokret660@gmail.com>
2025-06-07 14:10:54 +0200

    [5 19] add fun10.py

---
+++ day_5_7_06_2025/fun10.py
@@ -0,0 +1,46 @@
+import operator
+from functools import reduce, lru_cache
+
+
+def add(a, b):
+    return a + b
+
+
+# reduce ->  calculates ((((1 + 2) + 3) + 4) + 5)
+sum_all = reduce(add, [1, 2, 3])
+print(f"Reduce [sum_all]: {sum_all}")  # Reduce [sum_all]: 6 ((1 + 2) + 3)
+
+sum_all = reduce(lambda a, b: a + b, [1, 2, 3])
+print(f"Reduce [sum_all]: {sum_all}")  # Reduce [sum_all]: 6
+
+product = reduce(lambda a, b: a * b, [1, 2, 3, 4])
+print(f"Reduce [product]: {product}")  # Reduce [product]: 24
+
+lista = [1, 2, 3, 45, 67, 78, 100, 200, 300]
+
+sum_all = reduce(lambda a, b: a + b, list(map(lambda n: n * 2, list(filter(lambda n: n % 2 == 0, lista)))))
+print(f"Reduce:", sum_all)  # Reduce: 1360
+
+l_1 = [i for i in lista if i % 2 == 0]
+print(l_1)  # [2, 78, 100, 200, 300]
+l_2 = [i * 2 for i in l_1]
+print(l_2)  # [4, 156, 200, 400, 600]
+sum_all = reduce(lambda a, b: a + b, l_2)
+print(f"Reduce: {sum_all}")  # Reduce: 1360
+
+product = reduce(operator.mul, l_2)
+print(f"mul: {product}")  # mul: 29952000000
+
+add = reduce(operator.add, l_2)
+print(f"{add}")  # 1360
+
+concat_str = reduce(operator.add, ['Python', "rocks"])
+print(concat_str)  # Pythonrocks
+
+min_num = reduce(lambda a, b: a if a < b else b, l_2)
+print(f"Min num: {min_num}")  # Min num: 4
+
+print(reduce(lambda a, b: bool(a and b), [0, 0, 1, 0, 0]))  # False
+print(reduce(lambda a, b: bool(a and b), [0, 0, 0, 0, 0]))  # False
+print(reduce(lambda a, b: bool(a or b), [0, 0, 0, 0, 0]))  # False
+print(reduce(lambda a, b: bool(a or b), [0, 0, 1, 0, 0]))  # True
