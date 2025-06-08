794e36f37d06d7f4f4bb20460f37b47017745a8c
radoslawjaniak <rajkokret660@gmail.com>
2025-06-08 14:48:38 +0200

    [6 15] add kl2a.py

---
+++ day_6_8_06_2025/kl2a.py
@@ -0,0 +1,38 @@
+class Contact:
+    all_contacts = []  # pusta lista
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    # __repr__ rownież zmienia __str__
+    def __repr__(self):
+        return f"{self.name} {self.email}"
+
+
+class Suplier(Contact):
+    """
+    Klasa Suplier dziedziczy po klasie Contact
+    """
+
+    def order(self, order):
+        print(f"{order} zamówiono od {self.name}")
+
+
+c1 = Contact("Adam", "adam@wp.pl")
+c2 = Contact("Radek", "radek@wp.pl")
+c3 = Contact("Tomek", "tomek@wp.pl")
+print(c1.all_contacts)  # [Adam adam@wp.pl, Radek radek@wp.pl, Tomek tomek@wp.pl]
+print(c2.all_contacts)  # [Adam adam@wp.pl, Radek radek@wp.pl, Tomek tomek@wp.pl]
+print(c2.all_contacts)  # [Adam adam@wp.pl, Radek radek@wp.pl, Tomek tomek@wp.pl]
+
+print(Contact.all_contacts)  # [Adam adam@wp.pl, Radek radek@wp.pl, Tomek tomek@wp.pl]
+
+s1 = Suplier("Marek", "marek@o2.pl")
+print(Contact.all_contacts)  # [Adam adam@wp.pl, Radek radek@wp.pl, Tomek tomek@wp.pl, Marek marek@o2.pl]
+print(s1.all_contacts)  # [Adam adam@wp.pl, Radek radek@wp.pl, Tomek tomek@wp.pl, Marek marek@o2.pl]
+
+s1.order("kawa")  # kawa zamówiono od Marek
+
+# wypisz kontakty "Radek" z listy all_contacts
