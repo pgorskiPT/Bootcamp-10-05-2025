c8a5b5f7e731a7bcfa748af1d007d8653b434266
radoslawjaniak <rajkokret660@gmail.com>
2025-06-08 15:32:37 +0200

    [6 18] update kl4a.py

--- day_6_8_06_2025/kl4a.py
+++ day_6_8_06_2025/kl4a.py
@@ -16,5 +16,36 @@ print(d_python['name'])  # Radek
 d1 = DefaultDict()
 print(d1['name'])  # default - metoda __missing__ zwróciłą słówko "default"
 
+
 # zrobić słownik, który gdy nie ma klucza
 # dopisze ten klucz  z wartością 0
+class AutoDict(dict):
+    def __missing__(self, key):
+        self[key] = 0
+        return key
+
+
+d2 = AutoDict()
+print(d2)  # {}
+print(d2['name'])  # name
+print(d2)  # {'name': 0}
+print(d2['name'])  # 0
+d2['name'] = "Radek"
+print(d2)  # {'name': 'Radek'}
+
+
+class CaseInsensitiveDict(dict):
+    def __missing__(self, key):
+        if isinstance(key, str):  # sprawdza czy klucz jest typu str
+            return self.get(key.casefold())
+        return None
+
+
+d3 = CaseInsensitiveDict()
+d3['name'] = "Radek"
+print(d3['NAme'])  # Radek
+d3[1] = "Radek"
+print(d3)  # {'name': 'Radek', 1: 'Radek'}
+print(d3[2])  # AttributeError: 'int' object has no attribute 'casefold'
+# po dodaniu sprawdzania typu
+# None
