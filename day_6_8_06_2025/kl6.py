cfcc912ed773a053508d05a6c116c3d6046b845f
radoslawjaniak <rajkokret660@gmail.com>
2025-06-08 12:35:55 +0200

    [6 10] add kl6.py

--- day_6_8_06_2025/kl6.py
+++ day_6_8_06_2025/kl6.py
@@ -3,3 +3,16 @@
 # przedstaw_sie(), oblicz_pensje()
 # Zrobić kalsę Manager dziedziczącą po Pracownik
 # zastanowić sie co może dziedziczyć a co musi nadpisać ta klasa
+
+class Pracownik:
+
+    def __init__(self, imie, nazwisko, pensja):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.pensja = pensja
+
+    def przedstaw_sie(self):
+        print(f"Cześć, jestem {self.imie} {self.nazwisko}")
+
+    def oblicz_penja(self):
+        return self.pensja
