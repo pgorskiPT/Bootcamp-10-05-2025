c5f9b594d2906c571241597712d5e7b1bdfded3b
radoslawjaniak <rajkokret660@gmail.com>
2025-06-08 12:44:30 +0200

    [6 11] update kl6.py

--- day_6_8_06_2025/kl6.py
+++ day_6_8_06_2025/kl6.py
@@ -16,3 +16,32 @@ class Pracownik:
 
     def oblicz_penja(self):
         return self.pensja
+
+
+class Manager(Pracownik):
+    """
+    Klasa Manaer
+    """
+
+    def __init__(self, imie, nazwisko, pensja, premia):
+        super().__init__(imie, nazwisko, pensja)  # uruchomienie konstruktora z klasy wyższej (Pracownik)
+        self.premia = premia
+
+    def oblicz_penja(self):
+        return self.pensja + self.premia
+
+    pracownik = Pracownik("Jan", "Kowalski", 8000)
+    pracownik.przedstaw_sie()
+    wynagrodzenie_pracownika = pracownik.oblicz_penja()
+    print(f"Wynagrodzenie dla pracownika {pracownik.imie} {pracownik.nazwisko}: {wynagrodzenie_pracownika}")
+
+
+# Cześć, jestem Jan Kowalski
+# Wynagrodzenie dla pracownika Jan Kowalski: 8000
+
+manago = Manager("Anna", "Nowak", 12000, 3000)
+manago.przedstaw_sie()
+wynagrodzenie_manago = manago.oblicz_penja()
+print(f"Wynagrodzenie dla managera {manago.imie} {manago.nazwisko}: {wynagrodzenie_manago}")
+# Cześć, jestem Anna Nowak
+# Wynagrodzenie dla managera Anna Nowak: 15000
