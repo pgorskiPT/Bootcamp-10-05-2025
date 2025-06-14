d80dea720cf9ea967fe094b2e7fc20936491b588
radoslawjaniak <rajkokret660@gmail.com>
2025-06-14 10:53:30 +0200

    [7 8] add kl12a.py

--- day_7_14_06_2025/kl11a.py
+++ day_7_14_06_2025/kl11a.py
@@ -21,3 +21,29 @@ print(MyClass.counter)  # 5
 
 c2 = MyClass()
 print(c2.increment_counter())  # 6
+
+
+class Osoba:
+    def __init__(self, imie, nazwisko):
+        self.imie = imie
+        self.nazwisko = nazwisko
+
+    @classmethod
+    def z_nazwy_pelnej(cls, nazwa_pelna):
+        imie, nazwisko = nazwa_pelna.split()
+        return cls(imie, nazwisko)
+
+
+osoba1 = Osoba("Jan", "Kowalski")
+print(osoba1.imie, osoba1.nazwisko)
+# Jan Kowalski
+print("Jan Kowalski".split())  # ['Jan', 'Kowalski'] standardowo dzieli spacja
+imie, nazwisko = "Jan Kowalski".split()
+print(imie, ":", nazwisko)  # Jan : Kowalski
+print(" Jan : Kowalski".split(":"))  # [' Jan ', ' Kowalski']
+
+osoba2 = Osoba(imie, nazwisko)
+print(f"{osoba2.imie}, {osoba2.nazwisko}")  # Jan, Kowalski
+
+osoba3 = Osoba.z_nazwy_pelnej("Anna Nowak")
+print(f"{osoba3.imie}, {osoba3.nazwisko}")  # Anna, Nowak
