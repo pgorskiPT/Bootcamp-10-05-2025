fb4943faef98aa0dc5d9166ee8415a8aca2f4d54
radoslawjaniak <rajkokret660@gmail.com>
2025-06-14 14:36:48 +0200

    [7 21] add kl21a.py

---
+++ day_7_14_06_2025/kl21a.py
@@ -0,0 +1,76 @@
+# Stateless Validator + Cusutom Exceptions
+class MyError(Exception):
+    def __init__(self, message, err_code):
+        super().__init__(message)
+        self.err_code = err_code
+
+
+class MyValueError(MyError):
+    def __init__(self, message):
+        super().__init__(message, err_code=100)
+
+
+class MyTypeError(MyError):
+    def __init__(self, message):
+        super().__init__(message, err_code=200)
+
+
+class MyValidator:
+
+    @staticmethod
+    def is_int(value, name):
+        if not isinstance(value, int):
+            raise MyTypeError(f"{name} must be integer")
+
+    @staticmethod
+    def not_zero(value, name):
+        if value == 0:
+            raise MyValueError(f"{name} cannot be zero")
+
+
+# def my_function(x: int, y: int) -> float:
+#     if not isinstance(x, int):
+#         raise MyTypeError("x must be integer")
+#     if not isinstance(y, int):
+#         raise MyTypeError("y must be integer")
+#     if y == 0:
+#         raise MyValueError('y cannot be zero')
+#
+#     return x / y
+
+def my_function(x: int, y: int) -> float:
+    MyValidator.is_int(x, "x")
+    MyValidator.is_int(y, "y")
+    MyValidator.not_zero(y, "y")
+
+    return x / y
+
+
+try:
+    # result = my_function(4, 5)
+    # result = my_function(4, 0)
+    # result = my_function(0, 2)
+    result = my_function("Q", 6)
+except MyTypeError as e:
+    print("Caught a MyTypeError", e)
+    print("Error code:", e.err_code)
+except MyValueError as e:
+    print("Caught a MyValueError", e)
+    print("Error code:", e.err_code)
+except Exception as e:
+    print("Error:", e)
+else:
+    print(f"Result is: {result}")
+finally:
+    print("End")
+
+# Result is: 0.8
+# End
+
+# Caught a MyValueError
+# Error code: 100
+# End
+
+# Caught a MyTypeError
+# Error code: 200
+# End
