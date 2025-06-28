5b12da2f683d906c9ba059693fd47aa6bdb8e31b
radoslawjaniak <rajkokret660@gmail.com>
2025-06-28 16:04:12 +0200

    [9 24] add watki_3.py

---
+++ day_9_28-06-2025/watki3.py
@@ -0,0 +1,25 @@
+from concurrent.futures import ProcessPoolExecutor
+
+
+def worker(i):
+    print(f"Proces: {i}")
+
+
+print(__name__)  # __mp_main__
+
+# with ProcessPoolExecutor(max_workers=5) as executor:
+#     for i in range(20):
+#         executor.submit(worker, i)
+# #  This probably means that you are not using fork to start your
+#         child processes and you have forgotten to use the proper idiom
+#         in the main module:
+#
+#             if __name__ == '__main__':
+#                 freeze_support()
+#                 ...
+
+# przy ProcessPoolExecutor musimy uzywac __main__
+if __name__ == '__main__':
+    with ProcessPoolExecutor(max_workers=5) as executor:
+        for i in range(20):
+            executor.submit(worker, i)
