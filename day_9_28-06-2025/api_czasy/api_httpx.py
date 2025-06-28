6019e18edf083468a257cee014643d08bbc9163f
radoslawjaniak <rajkokret660@gmail.com>
2025-06-28 14:14:23 +0200

    [9 14] add api_httpx.py

---
+++ day_9_28-06-2025/api_czasy/api_httpx.py
@@ -0,0 +1,34 @@
+import time
+import httpx
+import asyncio
+
+url = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/"
+
+
+async def fetch_data(client, url, index):
+    start_time = time.time()
+    response = await client.get(url)
+    elapsed_time = time.time() - start_time
+    print(f"Request {index}: Status {response.status_code}, Time: {elapsed_time:.4f} s.")
+
+    try:
+        json_data = response.json()
+        print(f"Request {index}: Data {json_data}")
+    except httpx.HTTPStatusError as e:
+        print(f"Request {index}: Failed with status: {response.status_code}:", e)
+    except Exception as e:
+        print(f"Request {index}: Error {e}")
+
+
+async def multiple_httpx():
+    start_time = time.time()
+    async with httpx.AsyncClient() as client:
+        tasks = [fetch_data(client, url, i + 1) for i in range(100)]
+        await asyncio.gather(*tasks)
+
+    elapsed = time.time() - start_time
+    print(f"HTTPX total time: {elapsed:.4f} s.")
+
+
+# uruchomienie funkcji
+asyncio.run(multiple_httpx())
