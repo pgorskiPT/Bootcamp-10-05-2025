3ca082f5e79a478cc7065772a6592c1b4382c594
radoslawjaniak <rajkokret660@gmail.com>
2025-06-29 09:10:39 +0200

    [10 1] update watki_4.py

--- day_9_28-06-2025/watki4.py
+++ day_9_28-06-2025/watki4.py
@@ -2,14 +2,23 @@ import os
 import time
 from concurrent.futures import ProcessPoolExecutor
 
+
 def worker(n):
     print(f"Proces: {n} w PID {os.getpid()}")
     time.sleep(1)
+
 
 def main():
     with ProcessPoolExecutor(max_workers=5) as executor:
         for i in range(20):
             executor.submit(worker, i)
 
+
 if __name__ == '__main__':
     main()
+
+# Proces: 15 w PID 5982
+# Proces: 16 w PID 5986
+# Proces: 17 w PID 5983
+# Proces: 18 w PID 5985
+# Proces: 19 w PID 5984
