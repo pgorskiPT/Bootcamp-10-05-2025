4be17f8817e212a0d7ca19a01eea0dff4a1da80c
radoslawjaniak <rajkokret660@gmail.com>
2025-06-29 12:59:31 +0200

    [10 8] add baza4.py

---
+++ day_10_29_06_2025/baza4.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+sql_connection = None
+lista = []
+try:
+    # sql_connection = sqlite3.connect(":memory:") # baza umieszczona w pamięci
+    sql_connection = sqlite3.connect("sqlite_python.db")  # baza umieszczona w pamięci
+    sql_connection.row_factory = sqlite3.Row  # baza zwróci dane jako słownik
+    cursor = sql_connection.cursor()
+    print("Baza danych została podłączona")
+
+    select = """
+    SELECT * FROM software;
+    """
+
+    for row in cursor.execute(select):
+        print(row)  # <sqlite3.Row object at 0x102b57040>
+        lista.append(dict(row))  # sqlite3.Row pozwala zrobic z danych słownik
+
+    print(lista)
+    # [{'id': 1, 'name': 'Python', 'price': 100.0}, {'id': 2, 'name': 'Java', 'price': 1000.0},
+    #  {'id': 3, 'name': 'C++', 'price': 12000.0}]
+
+    cursor.execute(select)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(dict(row))
+    # {'id': 1, 'name': 'Python', 'price': 100.0}
+    # {'id': 2, 'name': 'Java', 'price': 1000.0}
+    # {'id': 3, 'name': 'C++', 'price': 12000.0}
+except sqlite3.Error as e:
+    print("Bład bazy danych:", e)
+finally:
+    if sql_connection:
+        sql_connection.close()
+        print("Baza danych została zamknięta")
