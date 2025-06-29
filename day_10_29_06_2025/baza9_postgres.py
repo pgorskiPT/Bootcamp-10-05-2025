76eb1cf93eebc84f1db3b8659373ebb118aaf562
radoslawjaniak <rajkokret660@gmail.com>
2025-06-29 16:13:13 +0200

    [10 15] update baza9_postgres.py.py

--- day_10_29_06_2025/baza9_postgres.py
+++ day_10_29_06_2025/baza9_postgres.py
@@ -3,8 +3,11 @@ import psycopg2
 
 # docker run --name my-postgres -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5432:5432 -d postgres
 # localhost - 127.0.0.1
+
+# psql -U myuser -d mydatabase -h localhost -p 5432
 # \l - lista baz
 # \q - wyjscie
+# \dt - lista tabel
 conn = psycopg2.connect(
     host="localhost",
     port=5432,
