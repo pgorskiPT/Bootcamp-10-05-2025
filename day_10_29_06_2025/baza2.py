4058364f27dfc31b6fc5af6744f62ea8769f17a1
radoslawjaniak <rajkokret660@gmail.com>
2025-06-29 12:22:35 +0200

    [10 5] add baza2.py

---
+++ day_10_29_06_2025/baza2.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+sql_connection = None
+
+try:
+    # sql_connection = sqlite3.connect(":memory:") # baza umieszczona w pamięci
+    sql_connection = sqlite3.connect("sqlite_python.db")  # baza umieszczona w pamięci
+    cursor = sql_connection.cursor()
+    print("Baza danych została podłączona")
+
+    query = '''
+    CREATE TABLE IF NOT EXISTS developers (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    joining_date DATETIME,
+    salary REAL NOT NULL
+    );
+    '''
+
+    cursor.execute(query)
+    sql_connection.commit()
+
+except sqlite3.Error as e:
+    print("Bład bazy danych:", e)
+finally:
+    if sql_connection:
+        sql_connection.close()
+        print("Baza danych została zamknięta")
