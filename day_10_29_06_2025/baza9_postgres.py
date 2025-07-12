f7e2096aeea6d37a3ffbbe793eb40e5dbc31c24d
radoslawjaniak <rajkokret660@gmail.com>
2025-07-12 09:55:00 +0200

    [11 1] update baza9_postgres.py.py

--- day_10_29_06_2025/baza9_postgres.py
+++ day_10_29_06_2025/baza9_postgres.py
@@ -1,4 +1,5 @@
 import psycopg2
+
 # pip install psycopg2
 
 # docker run --name my-postgres -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5432:5432 -d postgres
@@ -8,6 +9,12 @@ import psycopg2
 # \l - lista baz
 # \q - wyjscie
 # \dt - lista tabel
+
+# docker ps - lista uruchomionych kontenerów
+# docker ps -a - lista wszystkich kontenerów
+# docker start my-postgres - uruchomienie kontenera my-postgres
+# docker stop my-postgres - zatrzymanie kontenra dockera
+# pgAdmin - program do zarządzania baza postgres
 conn = psycopg2.connect(
     host="localhost",
     port=5432,
@@ -17,6 +24,21 @@ conn = psycopg2.connect(
 )
 
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);")
 
+cursor.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);")
 conn.commit()
+
+cursor.execute("INSERT INTO users (name) VALUES (%s)", ("Jan",))
+cursor.execute("INSERT INTO users (name) VALUES (%s)", ("Anna",))
+conn.commit()
+
+cursor.execute("SELECT * FROM users;")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.close()
+conn.close()
+# (1, 'Jan')
+# (2, 'Anna')
+# (3, 'Jan')
+# (4, 'Anna')
