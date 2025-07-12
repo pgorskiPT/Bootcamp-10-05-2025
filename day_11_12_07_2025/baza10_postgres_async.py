c3ec01c986979814d1159983c9fe7be20053f349
radoslawjaniak <rajkokret660@gmail.com>
2025-07-12 10:07:13 +0200

    [11 2] add baza10_postgres_async.py

---
+++ day_11_12_07_2025/baza10_postgres_async.py
@@ -0,0 +1,23 @@
+import psycopg2
+import asyncio
+import asyncpg
+
+
+async def run():
+    conn = await asyncpg.connect(
+        host="localhost",
+        port=5432,
+        database="mydatabase",
+        user="myuser",
+        password="mypassword"
+    )
+
+    await conn.fetch("CREATE TABLE IF NOT EXISTS persons(id SERIAL PRIMARY KEY, name TEXT);")
+    await conn.execute("""
+    INSERT INTO persons (name) VALUES ($1)
+    """, "Radek")
+    await conn.close()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
