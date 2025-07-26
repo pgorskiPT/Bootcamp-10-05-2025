a384ee6d2b076452f4a8ad691508137bd1b91a22
radoslawjaniak <rajkokret660@gmail.com>
2025-07-26 10:42:59 +0200

    [13 3] add okno_1.py

---
+++ day_13_26_07_2025/okno_1.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+# mvc
+
+# tworzenie okna
+root = tk.Tk()
+
+# ustawic tytuł okna
+root.title("My First Tkinter App")
+
+# wielkośc okna
+root.geometry("400x300")
+
+# uruchumienie okna
+root.mainloop()
