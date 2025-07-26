5d4018ab960c7679bad05885d3dfec3b401f9140
radoslawjaniak <rajkokret660@gmail.com>
2025-07-26 14:46:34 +0200

    [13 27] add okno17_kalendarz.py

--- day_13_26_07_2025/okno17_kalendarz.py
+++ day_13_26_07_2025/okno17_kalendarz.py
@@ -2,3 +2,26 @@ import tkinter as tk
 from tkinter import ttk
 from tkcalendar import Calendar
 from datetime import datetime
+
+
+class CustomEntry(ttk.Frame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master)
+        style = ttk.Style()
+        style.configure("Custom.TEntry", fieldbackground='#ffffff', foreground='#333333')
+        style.configure("Custom.TButton", fieldbackground='#dddddd', foreground='#333333')
+
+        self.var = tk.StringVar()
+        self.entry = ttk.Entry(self, textvariable=self.var,
+                               style='Custom.TEntry', width=16)
+        self.entry.pack(side="left", fill='x', expand=True)
+
+        self.btn = ttk.Button(self, text="▼", command=self.show_calendar,
+                              style="Custom.TButton", width=2)
+        self.btn.pack(side="left")
+
+        self.entry.bind('<Button-1>', lambda e: self.show_calendar())
+        self.calendar_win = None
+
+    def show_calendar(self):
+        pass
