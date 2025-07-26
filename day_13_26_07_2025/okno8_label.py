0ced7f8904d681cbae72e579c15b3826427e38f8
radoslawjaniak <rajkokret660@gmail.com>
2025-07-26 11:55:58 +0200

    [13 12a] add okno8_label.py

---
+++ day_13_26_07_2025/okno8_label.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Label Example")
+root.geometry("400x200")
+
+label = tk.Label(root, text="Hello, Tkinter!")
+
+label.pack()  # wyswietlenie na okienku
+
+root.mainloop()
