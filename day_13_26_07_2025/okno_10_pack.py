0733bcbddef92306e4288cebcfd7bde972df3bfd
radoslawjaniak <rajkokret660@gmail.com>
2025-07-26 12:12:52 +0200

    [13 15] add okno_10_pack.py

---
+++ day_13_26_07_2025/okno_10_pack.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Pack Layout Example")
+root.geometry("400x300")
+
+tk.Label(root, text="Top", bg="red").pack(side=tk.TOP, fill=tk.X)
+tk.Label(root, text="Bottom", bg="blue", fg="white").pack(side=tk.BOTTOM, fill=tk.X)
+tk.Label(root, text="Left", bg="green", fg="white").pack(side=tk.LEFT, fill=tk.Y)
+tk.Label(root, text="Right", bg="purple", fg="white").pack(side=tk.RIGHT, fill=tk.Y)
+
+tk.Label(root, text="Center",bg="yellow").pack(expand=True) # wycentrowana
+# tk.Label(root, text="Center",bg="yellow").pack()
+
+root.mainloop()
