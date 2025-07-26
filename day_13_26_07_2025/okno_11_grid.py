89fd637c65c2b28747683d16d10b8163680f9744
radoslawjaniak <rajkokret660@gmail.com>
2025-07-26 12:24:50 +0200

    [13 17] update okno_11_grid.py.py

--- day_13_26_07_2025/okno_11_grid.py
+++ day_13_26_07_2025/okno_11_grid.py
@@ -10,7 +10,8 @@ tk.Entry(root).grid(row=0, column=1, padx=5, pady=5)
 tk.Label(root, text="Last Name:").grid(row=1, column=0, padx=5, pady=5)
 #  sticky="ew" - pole entry wypełni cała dostępną przestrzeń
 tk.Entry(root).grid(row=1, column=1, padx=5, pady=5, sticky="ew")
-
+# ew - wschod, zachod
+# ns - połnoc, południe
 tk.Button(root, text="Submit").grid(row=2, column=0, columnspan=2, pady=10)
 
 # kolumna 1 moze byc zmieniana po zmianie szerokosci okna
