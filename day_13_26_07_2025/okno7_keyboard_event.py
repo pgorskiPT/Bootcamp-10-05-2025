39de8ec31a2cf08c0301c35696694825f342c768
radoslawjaniak <rajkokret660@gmail.com>
2025-07-26 11:49:57 +0200

    [13 11] add okno7_keyboard_event.py

---
+++ day_13_26_07_2025/okno7_keyboard_event.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+
+def on_key_press(event):
+    print(f"Key pressed: {event.char}")
+
+
+def on_return_key(event):
+    print("Return key pressed!!!")
+    text = entry.get()
+    print(f"Entered: {text}")
+
+
+root = tk.Tk()
+root.title("Keyboard Event Example")
+root.geometry("300x150")
+
+entry = tk.Entry(root)
+entry.pack(pady=20)
+entry.focus_set()  # ustawienie jako aktywne
+
+# podłaczenie funkcji pod naciśniecie klawisza
+entry.bind("<Key>", on_key_press)
+
+# podałczenie funkcji pod naciśnięcie klawisza return
+entry.bind("<Return>", on_return_key)
+
+tk.Label(root, text="Type in entry field and press Enter").pack()
+
+root.mainloop()
