a0d424de3b009adb772f186aa31a8bb968067447
radoslawjaniak <rajkokret660@gmail.com>
2025-07-26 11:21:09 +0200

    [13 8] add okno4_text.py

---
+++ day_13_26_07_2025/okno4_text.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+
+def show_text():
+    text = entry.get()
+    print(f"Wprowadzony tekst: {text}")
+
+
+app = tk.Tk()
+app.title("Pole wprowadzania")
+
+entry = tk.Entry(app)
+entry.pack()
+
+button = tk.Button(app, text="Pokaż tekst", command=show_text)
+button.pack()
+
+app.mainloop()
+# Wprowadzony tekst: tekst wpisany
