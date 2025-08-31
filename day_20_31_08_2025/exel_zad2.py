979e2c4a019f1a22c8d5bdb754031ed990babe89
radoslawjaniak <rajkokret660@gmail.com>
2025-08-31 14:50:15 +0200

    [20 20] add excel_zad2

---
+++ day_20_31_08_2025/exel_zad2.py
@@ -0,0 +1,29 @@
+# xlsxWriter - do tworzenia plików excel
+# pip install xlsxwriter
+
+import xlsxwriter
+import datetime as dt
+
+# tworzymy plik
+# book = xlsxwriter.Workbook("xlsxwriter.xlsx")
+book = xlsxwriter.Workbook("xlsxwriter2.xlsx")
+
+# tworzymy arkusz
+sheet = book.add_worksheet("Arkusz1")
+
+sheet.write("A1", 'Witaj1')
+sheet.write("A2", "Witaj 2")
+
+# formatowanie/kolory
+formatting = book.add_format(
+    {"font_color": "#FF0000",
+     "bg_color": "#FFFF00",
+     "bold": True,
+     "align": "center",
+     "border": 1,
+     "border_color": "#FF0000"}
+)
+sheet.write("A3", "Witaj 3", formatting)
+
+
+book.close()
