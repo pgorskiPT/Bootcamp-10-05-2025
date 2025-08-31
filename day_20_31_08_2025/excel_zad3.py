5eb102bbc4c82d92d40e7466fd4ff7db8acf14a0
radoslawjaniak <rajkokret660@gmail.com>
2025-08-31 17:15:29 +0200

    [20 33] fix for xlwt, old version installed

--- day_20_31_08_2025/excel_zad3.py
+++ day_20_31_08_2025/excel_zad3.py
@@ -1,7 +1,11 @@
 # pip install xlrd
 import xlrd
-
-# from xlwt.Utils import cell_to_rowcol2 # problem pythona 3.13
+import sys
+print(sys.version)      # pełny string, np. '3.13.2 (main, Aug  5 2025, ...)'
+print(sys.version_info)
+import sys
+print(sys.executable)
+from xlwt.Utils import cell_to_rowcol2 # problem pythona 3.13
 
 person = xlrd.open_workbook('dane_person.xls')
 
@@ -18,3 +22,4 @@ print(sheet.nrows)  # 2 wiersze
 print(sheet.ncols)  # 3 koluny
 
 print(sheet.cell(1, 0).value)  # Radek
+print(sheet.cell(*cell_to_rowcol2("B1")).value)
--- day_20_31_08_2025/excel_zad4.py
+++ day_20_31_08_2025/excel_zad4.py
@@ -1,6 +1,15 @@
 import xlwt
-import xlrd # z python 3.12
+import xlrd  # z python 3.12
 from xlutils.copy import copy
+#  pip uninstall xlwt
+# pip install "xlrd==1.2.0" "xlwt==1.3.0" "xlutils==2.0.0"
+import sys
+
+print(sys.version)  # pełny string, np. '3.13.2 (main, Aug  5 2025, ...)'
+print(sys.version_info)
+import sys
+
+print(sys.executable)
 
 book = xlrd.open_workbook("xlwt.xls", formatting_info=True)
 book = copy(book)
