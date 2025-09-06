3c62f4d391838958f334f247498b5b895eed4fa3
radoslawjaniak <rajkokret660@gmail.com>
2025-09-06 10:03:07 +0200

    [21 5] add excel_porownanie_plikow.py

---
+++ day_21_6_09_2025/excel_porownanie_plikow.py
@@ -0,0 +1,16 @@
+from openpyxl import load_workbook
+
+wb1 = load_workbook("tabela_przestawna.xlsx")
+wb2 = load_workbook("vgsales_formated.xlsx")
+
+ws1 = wb1.active
+ws2 = wb2.active
+
+print(ws1)
+print(ws2)
+
+if ws1['A1'].value == ws2['A1'].value:
+    print("Wartości A1 są identyczne")
+else:
+    print(f"Różnica: plik1={ws1['A1'].value}, plik2={ws2['A1'].value}")
+# Różnica: plik1=Data, plik2=Rank
