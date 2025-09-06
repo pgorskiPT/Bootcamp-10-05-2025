441184574e27561f43337eb56304a4b3a63a37df
radoslawjaniak <rajkokret660@gmail.com>
2025-09-06 11:56:20 +0200

    [21 17] add wykres plotly

---
+++ day_21_6_09_2025/wykres_plotly.py
@@ -0,0 +1,18 @@
+# import numpy as np
+# import plotly.express as px
+#
+# data = np.random.rand(10, 10)
+#
+# fig = px.imshow(data, color_continuous_scale="viridis", title="Heatmapa")
+#
+# fig.show()
+
+import numpy as np
+import plotly.express as px
+
+data = np.random.rand(10, 10)
+fig = px.imshow(data, color_continuous_scale="viridis", title="Heatmap")
+
+# fig.write_html("heatmap.html")
+
+fig.show()
