533ae5a7c56627b5a31fba5633c359a003fa4b07
radoslawjaniak <rajkokret660@gmail.com>
2025-09-06 14:41:58 +0200

    [21 25]add numpy_zad4_slice.py

---
+++ day_21_6_09_2025/numpy_zad4_slice.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+lista = [1, 2, 3, 4, 5]
+print(lista[2:4])  # [3, 4]
+
+# [start:stop]
+
+# indexy        0  1  2  3  4  5  6
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5])  # [2 3 4 5]
+print(arr[4:])  # [5 6 7] od indeksu 4 do końca włącznie
+print(arr[:4])  # [1 2 3 4] bez indeksu 4
+print(arr[-1:-3])  # [] -> 6:4
+print(arr[-3:-1])  # [5 6]
+print(arr[-3:])  # [5 6 7]
+
+# [start:stop:step]
+print(arr[1:5:2])  # [2 4]
+print(arr[::2])  # [1 3 5 7]
+
+arr_10_2d = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr_10_2d)
+# [[ 1  2  3  4  5]
+#  [ 6  7  8  9 10]]
+print("Wiersz indeks 1(element numer 2), element indeks 4 (czyli numer 5):", arr_10_2d[1, 1:4])
+# Wiersz indeks 1(element numer 2), element indeks 4 (czyli numer 5): [7 8 9]
+
+print(arr_10_2d[0:2, 2])  # [3 8], kolumna 2 z wszystkich wierszy
+
+print(arr_10_2d[0:2, 1:4])
+# [[2 3 4]
+#  [7 8 9]]
+
+array_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(array_3d)
+# [[[ 1  2  3]
+#   [ 4  5  6]]
+#
+#  [[ 7  8  9]
+#   [10 11 12]]]
+
+print(array_3d[0])
+# [[1 2 3]
+#  [4 5 6]]
+print(array_3d[0:2, 1:2, 0:1])
+# [[[ 4]]
+#
+#  [[10]]]
+
+
