007c1bd4422f30c2d2ee627c45d4b684aed65d23
radoslawjaniak <rajkokret660@gmail.com>
2025-09-07 12:05:03 +0200

    [22 11] add numpy_concatenate.py

---
+++ day_22_07_09_2025/numpy_concatenate.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr = np.concatenate((arr1, arr2))
+print(arr)  # [1 2 3 4 5 6]
+
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[5, 6], [7, 8]])
+print(arr1)
+# [[1 2]
+#  [3 4]]
+print(arr2)
+# [[5 6]
+#  [7 8]]
+arr = np.concatenate((arr1, arr2))
+print(arr)
+# [[1 2]
+#  [3 4]
+#  [5 6]
+#  [7 8]]
+arr = np.concatenate((arr1, arr2), axis=1)  # ułożone w poziomie
+print(arr)
+# [[1 2 5 6]
+#  [3 4 7 8]]
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.stack([arr1, arr2])  # wierszowo
+print(arr)
+# [[1 2 3]
+#  [4 5 6]]
+arr = np.stack([arr1, arr2], axis=1)  # kolumnowa
+print(arr)
+# [[1 4]
+#  [2 5]
+#  [3 6]]
+
+arr = np.hstack([arr1, arr2])  # łaczy poziomo
+print(arr)  # [1 2 3 4 5 6]
+
+arr = np.vstack([arr1, arr2])  # pionowo
+print(arr)
+# [[1 2 3]
+#  [4 5 6]]
+
+arr = np.dstack([arr1, arr2])  # głębokość, dodaje 3 wymiar
+print(arr)
+# [[[1 4]
+#   [2 5]
+#   [3 6]]]
+
+print(arr.shape)  # (1, 3, 2)
