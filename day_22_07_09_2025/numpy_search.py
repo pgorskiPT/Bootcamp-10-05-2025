8006ebe6b5f45581dba0f58903438d74eb199890
radoslawjaniak <rajkokret660@gmail.com>
2025-09-07 14:14:03 +0200

    [22 15] add numpy_search.py

---
+++ day_22_07_09_2025/numpy_search.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 4, 4])
+
+# zwróc indeksy elementów, które spełniają warunek
+x = np.where(arr == 4)
+print(x)  # (array([3, 5, 6]),)
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+x = np.where(arr % 2 == 0)
+print(x)  # (array([1, 3, 5, 7]),) indeks na których znajdują się liczby parzyste
+
+# gdzie moge wstawić 7 po lewej stronie
+arr = np.array([6, 7, 8, 9])
+x = np.searchsorted(arr, 7)
+print(x)
+
+# gdzie mogę wstawić po prawej stronie liczby 8 zachowując sortowanie
+arr = np.array([6, 7, 8, 9])
+x = np.searchsorted(arr, 8, side="right")
+print(x)  # 3
+
+arr = np.array([1, 3, 5, 7])
+x = np.searchsorted(arr, [2, 4, 6])
+print(x)  # [1 2 3] - pozycja dla każdej cyfry
+
+arr = np.array([1, 2, np.nan, 4, np.inf, 6, -np.inf, 8])
+# zamiana nan i inf na 0
+sanitized_arr = np.where(np.isfinite(arr), arr, 0)
+print(sanitized_arr)  # [1. 2. 0. 4. 0. 6. 0. 8.]
