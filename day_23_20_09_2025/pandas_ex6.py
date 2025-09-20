ba193c984a42f6ac5165be9685cc2dafdedf08ba
radoslawjaniak <rajkokret660@gmail.com>
2025-09-20 11:02:16 +0200

    [23 12] update pandas_ex6.py

--- day_23_20_09_2025/pandas_ex6.py
+++ day_23_20_09_2025/pandas_ex6.py
@@ -37,3 +37,67 @@ print(df.loc[141])
 # Maxpulse    127.0
 # Calories    130.0
 # Name: 141, dtype: float64
+
+# nowsze podejscie
+df = pd.read_csv('data.csv')
+df.fillna({"Calories": 130}, inplace=True)
+df.info()
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 169 entries, 0 to 168
+# Data columns (total 4 columns):
+#  #   Column    Non-Null Count  Dtype
+# ---  ------    --------------  -----
+#  0   Duration  169 non-null    int64
+#  1   Pulse     169 non-null    int64
+#  2   Maxpulse  169 non-null    int64
+#  3   Calories  169 non-null    float64
+# dtypes: float64(1), int64(3)
+# memory usage: 5.4 KB
+print(df.loc[141])
+# Duration     60.0
+# Pulse        97.0
+# Maxpulse    127.0
+# Calories    130.0
+# Name: 141, dtype: float64
+
+# bezpieczna metoda
+df = pd.read_csv('data.csv')
+df['Calories'] = df['Calories'].fillna(130)
+df.info()
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 169 entries, 0 to 168
+# Data columns (total 4 columns):
+#  #   Column    Non-Null Count  Dtype
+# ---  ------    --------------  -----
+#  0   Duration  169 non-null    int64
+#  1   Pulse     169 non-null    int64
+#  2   Maxpulse  169 non-null    int64
+#  3   Calories  169 non-null    float64
+# dtypes: float64(1), int64(3)
+# memory usage: 5.4 KB
+
+# df['Calories'].fillna(130, inplace=True)
+df.info()
+# /Users/radoslawjaniak/PycharmProjects/Bootcamp-10-05-2025/day_23_20_09_2025/pandas_ex6.py:79: FutureWarning: A value is trying to be set on a copy of a DataFrame or Series through chained assignment using an inplace method.
+# The behavior will change in pandas 3.0. This inplace method will never work because the intermediate object on which we are setting values always behaves as a copy.
+# print(df.loc[141])
+# Duration     60.0
+# Pulse        97.0
+# Maxpulse    127.0
+# Calories    130.0
+# Name: 141, dtype: float64
+
+# mean() - średnia arytmetyczna
+df = pd.read_csv('data.csv')
+
+x = df['Calories'].mean()
+print("Średnia wynosi:", x)  # Średnia wynosi: 375.79999999999995
+
+# zamiana NaN w danych na wartości średnie
+df['Calories'] = df['Calories'].fillna(x)
+print(df.loc[141])
+# Duration     60.0
+# Pulse        97.0
+# Maxpulse    127.0
+# Calories    375.8
+# Name: 141, dtype: float64
