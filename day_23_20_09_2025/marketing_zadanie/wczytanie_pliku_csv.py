e2c7ceb4ebc0ed4694f4c9132c0be303d52ab183
radoslawjaniak <rajkokret660@gmail.com>
2025-09-20 15:32:04 +0200

    [23 39] add wczytanie_pliku_csv.py

---
+++ day_23_20_09_2025/marketing_zadanie/wczytanie_pliku_csv.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+df = pd.read_csv('marketing_przecinek.csv', sep=",", nrows=1)
+print(df)
+print(df.columns)
+df.columns = df.columns.str.replace(" ", "")
+# Index(['          user_id', ' date_served', ' marketing_channel ',
+#        '         variant', ' converted ', 'language_displayed',
+#        ' language_preferred ', '   age_group', ' date_subscribed',
+#        ' date_canceled', ' subscribing_channel', ' is_retained'],
+#       dtype='object')
+# df_new = df.copy()
+# print(df_new)
+# print(df_new.columns)
+lista_kolumn = df.columns.tolist()
+print(lista_kolumn)
+# ['user_id', 'date_served',
+# 'marketing_channel', 'variant', 'converted',
+# 'language_displayed', 'language_preferred', 'age_group',
+# 'date_subscribed', 'date_canceled', 'subscribing_channel', 'is_retained']
+df = pd.read_csv('marketing_przecinek.csv',
+                 sep=r"\s{2,}",  # minimum dwa białe znaki (spacja, tabulator)
+                 engine="python",
+                 skiprows=1,
+                 header=None,
+                 names=lista_kolumn
+                 )
+
+print(df.head())
+print(df.head(1).to_string())
