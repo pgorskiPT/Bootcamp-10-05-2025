b4013a7758ad082d9379fb495d58fc8a330d4d7c
radoslawjaniak <rajkokret660@gmail.com>
2025-09-20 14:54:57 +0200

    [23 32] add excel_zip

--- day_23_20_09_2025/exel_zip.py
+++ day_23_20_09_2025/exel_zip.py
@@ -1,1 +1,14 @@
 from zipfile import ZipFile
+import xml.etree.ElementTree as ET
+# otwieramy plik Excel jako zip
+with ZipFile("tabela_przestawna2.xlsx", "r") as archive:
+    print(archive)
+    with archive.open("xl/worksheets/sheet1.xml") as f:
+        xml_content = f.read()
+
+print(xml_content)
+
+root = ET.fromstring(xml_content)
+
+shared_strings = [elem.text for elem in root.findall(".//{http://schemas.openxmlformats.org/spreadsheetml/2006/main}t")]
+print("Odczytane wartości tekstowe:", shared_strings)
