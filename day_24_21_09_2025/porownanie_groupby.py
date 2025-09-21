53057987a77a9012b08da95ac510e4ce3c94f195
radoslawjaniak <rajkokret660@gmail.com>
2025-09-21 15:56:48 +0200

    [24 28] update porownanie_groupby.py

--- day_24_21_09_2025/porownanie_groupby.py
+++ day_24_21_09_2025/porownanie_groupby.py
@@ -15,7 +15,8 @@ print("Czas:", time.time() - start)
 start = time.time()
 df = pl.read_csv(filename)
 result = df.group_by("category").agg(pl.col("value").sum())
-print("Polars groupby:", result)
+# print("Polars groupby:", result)
+print("Polars groupby:", result.to_pandas())
 print("Czas:", time.time() - start)
 
 # polars lazy, scan
@@ -28,7 +29,8 @@ result = (
     .agg(pl.col("value").sum())
     .collect()  # <-- TO JEST KLUCZ!
 )
-print("Polars groupby (lazy):", result)
+# print("Polars groupby (lazy):", result)
+print("Polars groupby (lazy):", result.to_pandas())
 print("Czas:", time.time() - start)
 
 # dask
@@ -50,3 +52,31 @@ print("Czas:", time.time() - start)
 # Czas: 1.9825000762939453 - polars
 # Czas: 2.427572011947632 - polars lazy
 # Czas: 4.624889135360718 - dask
+
+# wyświetlanie z polars dla to_pandas
+# Pandas groupby: category
+# A    125019660990
+# B    125004971437
+# C    125014117338
+# D    124996386888
+# Name: value, dtype: int64
+# Czas: 7.259937047958374
+# Polars groupby:   category         value
+# 0        A  125019660990
+# 1        B  125004971437
+# 2        C  125014117338
+# 3        D  124996386888
+# Czas: 2.0996429920196533
+# Polars groupby (lazy):   category         value
+# 0        C  125014117338
+# 1        B  125004971437
+# 2        D  124996386888
+# 3        A  125019660990
+# Czas: 2.4569168090820312
+# Dask groupby: category
+# A    125019660990
+# B    125004971437
+# C    125014117338
+# D    124996386888
+# Name: value, dtype: int64
+# Czas: 4.804033994674683
