7074fecc3685272a332fe951185fd5e0a8411dde
radoslawjaniak <rajkokret660@gmail.com>
2025-09-21 09:45:35 +0200

    [24 6] update marketing_zad2.py

--- day_23_20_09_2025/marketing_zadanie/marketing_zad2.py
+++ day_23_20_09_2025/marketing_zadanie/marketing_zad2.py
@@ -10,22 +10,23 @@ df = pd.read_csv('marketing_r.csv', sep=",")
 print(df['converted'].dtype)  # object
 
 df['is_house_ads'] = np.where(df['marketing_channel'] == "House Ads", True, False)
-print(df.is_house_ads.head(3))
+# print(df.is_house_ads.head(3))
 
 # zmienić typ kolumny na typ bool
 df['converted'] = df['converted'].astype('bool')
 print(df['converted'].dtype)  # bool
-df.info()
+
 
 df['date_served'] = pd.to_datetime(df['date_served'], errors='coerce', format='mixed')
-print(df['date_served'].head(3))
+# print(df['date_served'].head(3))
 
 channel_dict = {"House Ads": 1, "Instagram": 2, "Facebook": 3, "Email": 4, "Push": 5}
 df['channel_code'] = df['marketing_channel'].map(channel_dict)
-print(df['channel_code'].head(3))
+# print(df['channel_code'].head(3))
 
 daily_users = df.groupby(['date_served'])['user_id'].nunique()
-print("Dziennie:", daily_users)
+# print("Dziennie:", daily_users)
+df.info()
 
 df.to_csv("marketing_ok_date.csv")
 
