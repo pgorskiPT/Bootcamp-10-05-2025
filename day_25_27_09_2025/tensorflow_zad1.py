4a6dcefb7ba828826e4aed3838ea733eafc53438
radoslawjaniak <rajkokret660@gmail.com>
2025-09-27 15:47:20 +0200

    [25 26] add tensorflow_zad1.py

---
+++ day_25_27_09_2025/tensorflow_zad1.py
@@ -0,0 +1,12 @@
+import os
+import time
+import numpy as np
+
+# # pip install tensorflow
+# # pip uninstall tensorflow
+# # pip install tensorflow-cpu
+#
+# # pip install tensorflow-macos
+# # pip install tensorflow-metal (gpu)
+
+# os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
