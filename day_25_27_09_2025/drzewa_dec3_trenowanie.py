bd2f68e095a3414c2459314a2195459d3782af56
radoslawjaniak <rajkokret660@gmail.com>
2025-09-27 11:17:39 +0200

    [25 5] add drzewa_dec3_trenowanie.py

---
+++ day_25_27_09_2025/drzewa_dec3_trenowanie.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+X = np.array([
+    [25, 30],
+    [40, 70],
+    [35, 50],
+    [50, 90],
+    [20, 20],
+])
