9750d8c0fa3d6eb5d76364e244a7e06f001b82c0
radoslawjaniak <rajkokret660@gmail.com>
2025-09-27 11:30:09 +0200

    [25 6] update drzewa_dec3_trenowanie.py

--- day_25_27_09_2025/drzewa_dec3_trenowanie.py
+++ day_25_27_09_2025/drzewa_dec3_trenowanie.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
+# uczenie nadzorowane
+# [wiek, zarobki]
 X = np.array([
     [25, 30],
     [40, 70],
@@ -9,3 +11,22 @@ X = np.array([
     [50, 90],
     [20, 20],
 ])
+
+# etykiety: 1 -> tak, 0 -> nie
+y = np.array([0, 1, 1, 1, 0])
+
+# budowa i trenowanie drzewa
+tree = DecisionTreeClassifier(criterion='gini', max_depth=2)
+tree.fit(X, y)
+
+# predykcja
+decision = tree.predict(np.array([[30, 40]])) # Pożyczka odrzucona
+print("Pożyczka przyznana" if decision[0] == 1 else "Pożyczka odrzucona")
+
+# wizualizacja drzewa
+plt.figure(figsize=(8,5))
+plot_tree(tree, feature_names=['wiek', "zarobki"],
+          class_names=['odrzucona', 'przyznany'], filled=True)
+plt.title("Drzewo decyzyjne: przyznawanie pożyczki")
+plt.tight_layout()
+plt.show()
