0448e0d7a9149a8d795be4af3dbcf79f99695c4e
radoslawjaniak <rajkokret660@gmail.com>
2025-09-27 15:57:09 +0200

    [25 27] update tensorflow_zad1.py

--- day_25_27_09_2025/tensorflow_zad1.py
+++ day_25_27_09_2025/tensorflow_zad1.py
@@ -10,3 +10,9 @@ import numpy as np
 # # pip install tensorflow-metal (gpu)
 
 # os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+# 3.13.7
+import tensorflow as tf
+
+print("Dostępne urządzenia:")
+print(tf.config.list_physical_devices())
