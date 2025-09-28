c7b94c4e89b4f77c0607cec2904b7fa6cb88717f
radoslawjaniak <rajkokret660@gmail.com>
2025-09-28 12:55:37 +0200

    [26 24] add ollama_zad1.py

---
+++ day_26_28_09_2025/ollama_zad1.py
@@ -0,0 +1,15 @@
+import requests
+
+# ollama pull SpeakLeash/bielik-11b-v2.3-instruct:Q4_K_M
+# ollama run SpeakLeash/bielik-11b-v2.3-instruct:Q4_K_M
+
+url = "http://localhost:11434/api/generate"
+headers = {"Content-Type": "application/json"}
+data = {
+    "model": "SpeakLeash/bielik-11b-v2.3-instruct:Q4_K_M",
+    "prompt": "Wymień trzy największe miasta w Polsce.",
+    "stream": False
+}
+
+response = requests.post(url, json=data, headers=headers)
+print(response.json()["response"])
